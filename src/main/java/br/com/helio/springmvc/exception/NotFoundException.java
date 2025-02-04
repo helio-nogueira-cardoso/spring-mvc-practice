b95fe59dfe38503337ae82377b13e3b36fa5f03b
Hélio Nogueira Cardoso <rinder_omna@outlook.com>
2025-02-04 14:05:21 -0300

    Now handling exception with just a @ResponseStatus annotation above custom exception

--- src/main/java/br/com/helio/springmvc/exception/NotFoundException.java
+++ src/main/java/br/com/helio/springmvc/exception/NotFoundException.java
@@ -1,4 +1,8 @@
 package br.com.helio.springmvc.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Value not found")
 public class NotFoundException extends RuntimeException {
 }
