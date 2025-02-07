ed5f12f7c51703f49fa91c0661d3316df0b7ff00
Hélio Nogueira Cardoso <rinder_omna@outlook.com>
2025-02-07 11:44:44 -0300

    Adding MySQL driver

--- src/main/java/br/com/helio/springmvc/entities/Customer.java
+++ src/main/java/br/com/helio/springmvc/entities/Customer.java
@@ -5,6 +5,8 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.*;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
@@ -15,9 +17,12 @@ import java.util.UUID;
 @AllArgsConstructor
 @Builder
 @Entity
+@Table(name = "customer")
 public class Customer {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
+    @JdbcTypeCode(SqlTypes.CHAR)
+    @Column(length = 36, columnDefinition = "varchar(36)", updatable = false, nullable = false)
     private UUID id;
 
     @NotBlank
