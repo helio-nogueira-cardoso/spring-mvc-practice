a07d2f1ac4bc3cb929ac583d2bebc3a550d06dbf
Hélio Nogueira Cardoso <rinder_omna@outlook.com>
2025-02-13 09:48:25 -0300

    Setting HTTP Basic Authentication up

--- src/main/java/br/com/helio/springmvc/SpringMvcApplication.java
+++ src/main/java/br/com/helio/springmvc/SpringMvcApplication.java
@@ -2,8 +2,12 @@ package br.com.helio.springmvc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+
+import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
 
 @SpringBootApplication
+@EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
 public class SpringMvcApplication {
 
     public static void main(String[] args) {
